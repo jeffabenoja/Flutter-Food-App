1b403f811bce30e0fe6e87bd2c8fce86567fc46e
Renz Jefferson A. Padro <jeffabenoja18@gmail.com>
2023-11-14 23:26:34 +0800

    Create new components

---
+++ lib/Widgets/MostOrderWidget.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class MostOrderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 15,
+          horizontal: 5,
+        ),
+        child: Row(
+          children: [
+            // Single Item
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 7),
+              child: Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      ),
+                    ]),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset(
+                          "images/pizza/pizza2.png",
+                          height: 130,
+                        ),
+                      ),
+                      Text(
+                        "Add Name",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        "Add Description",
+                        style: TextStyle(
+                          fontSize: 12,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 7),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\Php 350.00",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 18,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 7),
+              child: Container(
+                width: 170,
+                height: 225,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      ),
+                    ]),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: Image.asset(
+                          "images/burger/burger2.png",
+                          height: 130,
+                        ),
+                      ),
+                      Text(
+                        "Add Name",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        "Add Description",
+                        style: TextStyle(
+                          fontSize: 12,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 7),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\Php 180.00",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.red,
+                            size: 18,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // Next Component
+          ],
+        ),
+      ),
+    );
+  }
+}
